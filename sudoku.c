2e5f8ba5a465abe21907519d34c702993df5f3ae
Claudio Almeida Santos Junior <cla.junior21@gmail.com>
2022-03-08 16:14:16 -0300

    reset

--- sudoku.c
+++ sudoku.c
@@ -2,11 +2,20 @@
 
 void print_matriz(int matriz[9][9]){
     int i,j,a=1,b=1;
+    for (i = 1; i < 10; i++){
+        if (i == 4 || i == 7)
+            printf("\t");
+        printf("\t%i", i);
+    }
+
+    printf("\n  -----------------------------------------------------------------------------------------------\n");
+    
     for (i=0;i<9;i++){
+        printf("%i |", i+1);
         for(j=0;j<9;j++){
-            printf("(%i)",matriz[i][j]);
+            printf("\t%i",matriz[i][j]);
             if (a==3){
-                printf("|");
+                printf("\t|");
                 a=1;
             } else{
                 a++;
@@ -14,7 +23,7 @@ void print_matriz(int matriz[9][9]){
         }
         printf("\n");
         if(b==3){
-            printf("------------------------------\n");
+            printf("  -----------------------------------------------------------------------------------------------\n");
             b=1;
         }else{
             b++;
@@ -23,88 +32,75 @@ void print_matriz(int matriz[9][9]){
     printf("\n");
 }
 
-int resultado(int matriz[9][9]){
-    int resultado=1;
-    int i,j,k;
-    int teste;
-    //checar linhas
-    for (i=0;i<9;i++){
-        for(j=0;j<9;j++){
-           for (k=0;k<9;k++){
-               if( matriz[i][j]=matriz[i][k] && j!=k){
-                   resultado = 0;
-                   return resultado;
-               }
-           }
-            
-        }
-    }
-
-    //checar coluna
-    for (i=0;i<9;i++){
-        for(j=0;j<9;j++){
-           for (k=0;k<9;k++){
-               if( matriz[j][i]=matriz[j][k] && i!=k){
-                   resultado = 0;
-                   return resultado;
-               }
-           }
-            
-        }
-    }
-    //checar quadrado
-    return resultado;
-}
-
 int main (){
     //matriz
-    int sudoku[9][9] = {     {0, 0, 5, 3, 0, 0, 0, 0, 0},
-                             {8, 0, 0, 0, 0, 0, 0, 2, 0},
-                             {0, 7, 0, 0, 1, 0, 5, 0, 0},
-                             {4, 0, 0, 0, 0, 5, 3, 0, 0},
-                             {0, 1, 0, 0, 7, 0, 0, 0, 6},
-                             {0, 0, 3, 2, 0, 0, 0, 8, 0},
-                             {0, 6, 0, 5, 0, 0, 0, 0, 9},
-                             {0, 0, 4, 0, 0, 0, 0, 3, 0},
-                             {0, 0, 0, 0, 0, 9, 7, 0, 0}};
-    print_matriz(sudoku);
+    int sudokuFacil[9][9] = {{0, 0, 0, 0, 0, 0, 0, 0, 0},
+                             {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                             {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                             {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                             {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                             {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                             {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                             {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                             {0, 0, 0, 0, 0, 0, 0, 0, 0}};
+    
+    int sudokuMedio[9][9] = {{0, 0, 0, 0, 0, 0, 0, 0, 0},
+                             {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                             {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                             {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                             {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                             {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                             {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                             {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                             {0, 0, 0, 0, 0, 0, 0, 0, 0}};
+    
+    int sudokuDificil[9][9] = {{0, 0, 0, 0, 0, 0, 0, 0, 0},
+                             {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                             {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                             {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                             {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                             {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                             {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                             {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                             {0, 0, 0, 0, 0, 0, 0, 0, 0}};
+
+
+    int sudoku[9][9];
+    int dificuldade;
+    printf("Nivel do jogo: 1- Facil, 2-Media, 3- Dificil");
+    scanf("%i,&dificuldade")
+    switch (dificuldade)
+    {
+    case 1:
+        sudoku=sudokuFacil;
+        break;
+    case 2:
+        sudoku=sudokuMedio;
+        break;
+    case 3:
+        sudoku=sudokuDificil;
+        break;
+    
+    default:
+        printf(" Valor Invalido");
+        break;
+    }
+
+
+    
 
     //jogo
     int loop = 1;
     int comando,a,b,c;
-    while (loop == 1)
-    {
-        printf("\n\n=============================================================");
-        printf("\n Comandos:\n 1- Imprimir tabela\n 2- Alterar algum valor\n 3- Checar Resultado\n");
-        scanf("%i",&comando);
-
-        switch (comando)
-        {
-        case 1:
-            print_matriz(sudoku);
-            break;
-        case 2:
-            printf("\n Coordenada da celula vc quer mudar?");
-            scanf("%i %i", &a, &b);
-            printf("\n Valor:");
-            scanf("%i", &c);
-            sudoku[a][b]=c;
-            break;
-        case 3:
-            if(resultado(sudoku)==1){
-                printf("\nParabens! Vc terminou.\t:)");
-            } else {
-                printf("\nAinda nao acabou...\n");
-            }
-            break;
-        default:
-        printf("Comando Invalido!");
-            break;
-        }
-
+    while (loop == 1){
+        print_matriz(sudoku);
+        printf("coordenada para ser alterada: "); scanf("%i % i",&a,&b);
+        printf("Valor: "); scanf("%i",&c);
+        
+        
+        
     }
     
    
     return 0;
 }
- 
