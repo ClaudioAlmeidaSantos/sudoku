033f8a7e2f37275328bb01e7e590eae50de28af8
Claudio  Junior <cla.junior21@gmail.com>
2022-03-11 16:05:34 -0300

    add matriz gg

--- sudoku.c
+++ sudoku.c
@@ -126,6 +126,16 @@ int main (void){
                                 {0, 6, 0, 0, 0, 0, 0, 0, 0},
                                 {0, 0, 0, 1, 4, 7, 0, 0, 0},
                                 {0, 1, 4, 8, 3, 6, 0, 0, 5}};
+    
+    int sudokuGG[9][9] = {      {0, 3, 4, 6, 7, 8, 9, 1, 2},
+                                {6, 7, 2, 1, 9, 5, 3, 4, 8},
+                                {1, 9, 8, 3, 4, 2, 5, 6, 7},
+                                {8, 5, 9, 7, 6, 1, 4, 2, 3},
+                                {4, 2, 6, 8, 5, 3, 7, 9, 1},
+                                {7, 1, 3, 9, 2, 4, 8, 5, 6},
+                                {9, 6, 1, 5, 3, 7, 2, 8, 4},
+                                {2, 8, 7, 4, 1, 9, 6, 3, 5},
+                                {3, 4, 5, 2, 8, 6, 1, 7, 9}};
 
     // recebendo de uma matriz base
     int sudoku[9][9];
@@ -166,8 +176,16 @@ int main (void){
             case 4:
                 printf("Programa encerrado!\n");        
                 return 0;
+            case 5:
+                for (i=0;i<9;i++){
+                    for(j=0;j<9;j++){
+                        sudoku[i][j] = sudokuGG[i][j];
+                        base[i][j] = sudokuGG[i][j];
+                    }
+                }
+                break;
         }
-        if (dif < 1 || dif > 4)
+        if (dif < 1 || dif > 5)
             printf("\nValor invalido\n");
         else
             break;
